23869f2c79eed00425f18db6955f9e5d2c2606ab
shiweisen <shiweisen@bluemoon.com.cn>
2019-06-04 09:25:04 +0800

    [add] first commit

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import NotFound from "./views/404";
 
 Vue.use(Router);
 
@@ -14,6 +15,29 @@ export default new Router({
       component: Home
     },
     {
+      path: "/user",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./layouts/UserLayout"),
+      children: [
+        {
+          path: "/user",
+          redirect: "/user/login"
+        },
+        {
+          path: "/user/login",
+          name: "login",
+          component: () =>
+            import(/* webpackChunkName: "user" */ "./views/user/Login")
+        },
+        {
+          path: "/user/register",
+          name: "register",
+          component: () =>
+            import(/* webpackChunkName: "user" */ "./views/user/Register")
+        }
+      ]
+    },
+    {
       path: "/about",
       name: "about",
       // route level code-splitting
@@ -21,6 +45,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "*",
+      name: "404",
+      component: NotFound
     }
   ]
 });
